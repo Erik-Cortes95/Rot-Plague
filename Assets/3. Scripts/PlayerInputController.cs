a986c8f8d0b0b94ea5342365d4b6f2155e04d218
adrianlandete <adrianlandete1212@gmail.com>
2025-03-05 11:30:57 +0100

    modelos armas+ meter interfaz en la escena principal

--- Assets/3. Scripts/PlayerInputController.cs
+++ Assets/3. Scripts/PlayerInputController.cs
@@ -4,7 +4,6 @@ using UnityEngine.InputSystem;
 public class PlayerInputController : MonoBehaviour
 {
     [SerializeField] private Pistol pistol;
-    [SerializeField] private AssaultRifle assaultRifle;
     [SerializeField] private Shotgun shotgun;
 
     private void OnShoot(InputValue value)
@@ -12,7 +11,6 @@ public class PlayerInputController : MonoBehaviour
         if (value.isPressed)
         {
             pistol.OnShoot();
-            assaultRifle.OnShoot(value);
             shotgun.OnShoot();
         }
     }
