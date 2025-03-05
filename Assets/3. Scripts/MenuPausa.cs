84f708367078b19cc696e2dec3eb549522e809a8
Aldope33 <albadp03@gmail.com>
2025-03-05 15:50:38 +0100

    Menu de pausa + cambio de escenas; funciona correctamente

--- Assets/3. Scripts/MenuPausa.cs
+++ Assets/3. Scripts/MenuPausa.cs
@@ -1,26 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuPausa : MonoBehaviour
 {
     public GameObject ObjetoMenuPausa;
     public bool Pausa = false;
-    
+
     void Start()
     {
-
+        
     }
 
-    private void Update()
+    void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.P))
         {
             if(Pausa == false)
             {
                 ObjetoMenuPausa.SetActive(true);
                 Pausa = true;
+
+                Time.timeScale = 0;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+
+                AudioSource[] sonidos = FindObjectsOfType<AudioSource>();
+
+                for(int i = 0; i < sonidos.Length; i++)
+                {
+                    sonidos[i].Pause();
+                }
+            }
+            else if(Pausa == true)
+            {
+                Resumir();
             }
         }
     }
+
+    public void Resumir()
+    {
+        ObjetoMenuPausa.SetActive(false);
+        Pausa = false;
+
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        AudioSource[] sonidos = FindObjectsOfType<AudioSource>();
+
+        for (int i = 0; i < sonidos.Length; i++)
+        {
+            sonidos[i].Play();
+        }
+    }
+    public void MainMenu(string NombreMenu)
+    {
+        SceneManager.LoadScene(NombreMenu);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
 }
+
